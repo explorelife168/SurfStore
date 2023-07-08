15f76015c703467a653fafaf265899750890d865
Neil Wu <Neil@fakemail.com>
2023-07-08 17:28:38 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 # git push -f https://github.com/explorelife168/SurfClone.github.io.git master:gh-pages
-git push -f git@github.com:explorelife168/SurfClone.github.io.git master:gh-pages
+git push -f git@github.com:explorelife168/SurfStore.io.git master:gh-pages
 
 cd -
