9d3acaa531a930da5804d9e9ff5738a67ad762ba
Neil Wu <Neil@fakemail.com>
2023-07-08 11:15:58 +0800

    0708 1115

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-});
 
-module.exports = {
-  publicPath: "/SurfClone.github.io/",
-};
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/SurfClone.github.io//" : "/",
+});
