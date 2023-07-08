dcf3fad6d785c10a08e7b18ea857fcc1b9d57dbf
Neil Wu <Neil@fakemail.com>
2023-07-08 22:41:47 +0800

    future picture change

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-# git push -f https://github.com/explorelife168/SurfClone.github.io.git master:gh-pages
-git push -f git@github.com:explorelife168/SurfStore.io.git master:gh-pages
+git push -f https://github.com/explorelife168/SurfStore.git master:gh-pages
+# git push -f git@github.com:explorelife168/SurfStore.git master:gh-pages
 
 cd -
